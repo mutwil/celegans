763253fa0da61be00843de05cd86af86ef82dae0
Your Name <you@example.com>
2023-04-20 19:43:50 +0800

    Your commit message here

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def find_terms(my_search, genes):
                     for j in genes[i]:
                         if j[0]!='' and j[2]!='':
                             forSending.append(Gene(j[0], j[2], j[1], j[3])) #source, target, type
-                            elements.append({"source": j[0].replace("'","").replace('"',''), "target": j[2].replace("'","").replace('"',''), "interaction": j[1]})                
+                            elements.append({"source": j[0].replace("'","").replace('"',''), "target": j[2].replace("'","").replace('"',''), "interaction": j[1].replace("'","").replace('"','')})                
             if my_search.upper().strip() in i.strip():
                 print(i,'asd')
                 for j in genes[i]:
